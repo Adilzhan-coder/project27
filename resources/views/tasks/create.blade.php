106f3325142f65f33560b0b7d64d2ffea31f4c74
Adil <angrybirds.2000.ru>
2019-07-04 20:49:46 -0400

    Initial Commit

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('title','Create Task')
+
+@section('content')
+
+   <div class="row">
+    <col-wm-12>
+    <h1>Create Task</h1>
+    </col-wm-12>
+    </div class="row">
+    @endsection
